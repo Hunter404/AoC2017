23c6de650faed77fae5f19a79447e7feb28a2116
Fredrik Larsson <hunter404@live.se>
2017-12-05 00:53:58 +0100

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,7 +1,17 @@
+/**
+ * Splits a string into an array of numbers
+ * param {String} string of numbers
+ * return {Array} array of numbers
+ */
 function stringToNumbers(string) {
     return string.split('').map(c => Number(c));
 }
 
+/**
+ * Calculate the captcha summary from an array of numbers
+ * param {Array} array of numbers
+ * return {Number} captcha
+ */
 function captchaSum(captcha) {
     return captcha.filter((i, index) => i === captcha[index + 1 >= captcha.length ? 0 : index + 1]).reduce((sum, val) => sum + val);
 }
